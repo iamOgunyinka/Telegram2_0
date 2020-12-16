89530d0695dd8bfecaa5b104231fa2da627c8810
Joshua Ogunyinka <ogunyinkajoshua@yahoo.com>
2020-12-16 23:25:41 +0400

    Renamed program to v4

--- sdi_zh_CN.ts
+++ sdi_zh_CN.ts
@@ -4,241 +4,306 @@
 <context>
     <name>AccountWidget</name>
     <message>
+        <location filename="accountwidget.ui" line="25"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>形成</translation>
     </message>
     <message>
+        <location filename="accountwidget.cpp" line="53"/>
+        <location filename="accountwidget.cpp" line="122"/>
         <source>Select all</source>
-        <translation type="unfinished"></translation>
+        <translation>全选</translation>
     </message>
 </context>
 <context>
     <name>MainDialog</name>
     <message>
+        <location filename="maindialog.ui" line="19"/>
         <source>Telegram Search(v4.0)</source>
-        <translation type="unfinished"></translation>
+        <translation>电报搜索（v4.0）</translation>
     </message>
     <message>
+        <location filename="maindialog.ui" line="46"/>
         <source>Select all</source>
-        <translation type="unfinished"></translation>
+        <translation>全选</translation>
     </message>
     <message>
+        <location filename="maindialog.ui" line="83"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>删除帐户</translation>
     </message>
     <message>
+        <location filename="maindialog.ui" line="96"/>
+        <location filename="maindialog.cpp" line="547"/>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>登录</translation>
     </message>
     <message>
+        <location filename="maindialog.ui" line="109"/>
         <source>New user</source>
-        <translation type="unfinished"></translation>
+        <translation>新增电话号码</translation>
     </message>
     <message>
+        <location filename="maindialog.ui" line="122"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>注销帐户</translation>
     </message>
     <message>
+        <location filename="maindialog.ui" line="141"/>
         <source>Stop scheduler</source>
-        <translation type="unfinished"></translation>
+        <translation>停止后台搜索</translation>
     </message>
     <message>
+        <location filename="maindialog.ui" line="154"/>
         <source>Start scheduled task</source>
-        <translation type="unfinished"></translation>
+        <translation>开始后台搜索</translation>
     </message>
     <message>
+        <location filename="maindialog.ui" line="180"/>
+        <location filename="maindialog.cpp" line="89"/>
+        <location filename="maindialog.cpp" line="92"/>
+        <location filename="maindialog.cpp" line="230"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索</translation>
     </message>
     <message>
+        <location filename="maindialog.ui" line="209"/>
         <source>Export</source>
-        <translation type="unfinished"></translation>
+        <translation>保存结果</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="89"/>
         <source>No result found</source>
-        <translation type="unfinished"></translation>
+        <translation>找不到结果</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="92"/>
         <source>There was an error on the server-side, so search could not be completed</source>
-        <translation type="unfinished"></translation>
+        <translation>服务器端发生错误，因此无法完成搜索</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="102"/>
         <source>%1(%2)(%3): %4</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="107"/>
         <source>%1 items found for %2</source>
-        <translation type="unfinished"></translation>
+        <translation>找到%1个项目，共%2个</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="122"/>
+        <location filename="maindialog.cpp" line="467"/>
+        <location filename="maindialog.cpp" line="566"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="187"/>
         <source>Task</source>
-        <translation type="unfinished"></translation>
+        <translation>任务</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="187"/>
         <source>You have a background search ongoing, suspend it first</source>
-        <translation type="unfinished"></translation>
+        <translation>您正在后台搜索，请先将其暂停</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="231"/>
         <source>Choose an account(online) to conduct a search on</source>
-        <translation type="unfinished"></translation>
+        <translation>选择一个帐户（在线）进行搜索</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="239"/>
         <source>%1h %2m %3s</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="247"/>
         <source>Interval: %1s</source>
-        <translation type="unfinished"></translation>
+        <translation>间隔：%1s</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="325"/>
         <source>Directory</source>
-        <translation type="unfinished"></translation>
+        <translation>夹</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="346"/>
         <source>Status</source>
-        <translation type="unfinished"></translation>
+        <translation>状态</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="346"/>
         <source>File(s) saved successfully</source>
-        <translation type="unfinished"></translation>
+        <translation>文件已成功保存</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="362"/>
         <source>Background search stopped</source>
-        <translation type="unfinished"></translation>
+        <translation>后台搜索已停止</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="377"/>
         <source>Unable to write to `result.xlsx`</source>
-        <translation type="unfinished"></translation>
+        <translation>无法写入Excel文件</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="383"/>
         <source>S/N</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="384"/>
         <source>Group/Chat name</source>
-        <translation type="unfinished"></translation>
+        <translation>组名</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="385"/>
         <source>Sender name</source>
-        <translation type="unfinished"></translation>
+        <translation>发件人名称</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="386"/>
         <source>@username(sender)</source>
-        <translation type="unfinished"></translation>
+        <translation>发件人的用户名</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="387"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>信息</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="388"/>
         <source>Phone number</source>
-        <translation type="unfinished"></translation>
+        <translation>电话号码</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="390"/>
         <source>Keyword</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索词</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="422"/>
         <source>Confirmation</source>
-        <translation type="unfinished"></translation>
+        <translation>确认书</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="423"/>
         <source>Are you sure you want to delete these accounts?</source>
-        <translation type="unfinished"></translation>
+        <translation>您确定要删除这些帐户吗？</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="467"/>
         <source>Unable to save login information</source>
-        <translation type="unfinished"></translation>
+        <translation>无法保存登录信息</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="513"/>
         <source>Request</source>
-        <translation type="unfinished"></translation>
+        <translation>请求</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="514"/>
         <source>Enter authorization code for %1</source>
-        <translation type="unfinished"></translation>
+        <translation>输入%1的授权码</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="524"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>密码</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="525"/>
         <source>Enter authorization password for %1</source>
-        <translation type="unfinished"></translation>
+        <translation>输入%1的授权密码</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="547"/>
         <source>Please manually login account</source>
-        <translation type="unfinished"></translation>
+        <translation>请手动登录帐号</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="560"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>完成了</translation>
     </message>
     <message>
+        <location filename="maindialog.cpp" line="560"/>
         <source>All accounts with green button are logged in</source>
-        <translation type="unfinished"></translation>
+        <translation>所有带有绿色按钮的帐户均已登录</translation>
     </message>
 </context>
 <context>
     <name>RegistrationDialog</name>
     <message>
+        <location filename="registrationdialog.ui" line="19"/>
         <source>New user</source>
-        <translation type="unfinished"></translation>
+        <translation>新增电话号码</translation>
     </message>
     <message>
+        <location filename="registrationdialog.ui" line="51"/>
         <source>Register</source>
-        <translation type="unfinished"></translation>
+        <translation>寄存器</translation>
     </message>
     <message>
+        <location filename="registrationdialog.ui" line="67"/>
         <source>Phone number</source>
-        <translation type="unfinished"></translation>
+        <translation>电话号码</translation>
     </message>
     <message>
+        <location filename="registrationdialog.cpp" line="19"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
+        <location filename="registrationdialog.cpp" line="19"/>
         <source>Cannot leave this field empty</source>
-        <translation type="unfinished"></translation>
+        <translation>电话号码字段不能为空</translation>
     </message>
 </context>
 <context>
     <name>SchedulerDialog</name>
     <message>
+        <location filename="schedulerdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>背景搜索</translation>
     </message>
     <message>
+        <location filename="schedulerdialog.ui" line="22"/>
         <source>Timer(seconds)</source>
-        <translation type="unfinished"></translation>
+        <translation>计时器（以秒为单位）</translation>
     </message>
     <message>
+        <location filename="schedulerdialog.ui" line="32"/>
         <source>Search term</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索什么</translation>
     </message>
     <message>
+        <location filename="schedulerdialog.ui" line="46"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>好</translation>
     </message>
     <message>
+        <location filename="schedulerdialog.ui" line="53"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>取消</translation>
     </message>
     <message>
+        <location filename="schedulerdialog.cpp" line="17"/>
+        <location filename="schedulerdialog.cpp" line="24"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
+        <location filename="schedulerdialog.cpp" line="17"/>
         <source>This cannot be empty</source>
-        <translation type="unfinished"></translation>
+        <translation>该字段不能为空</translation>
     </message>
     <message>
+        <location filename="schedulerdialog.cpp" line="24"/>
         <source>Invalid number</source>
-        <translation type="unfinished"></translation>
+        <translation>手机号码无效</translation>
     </message>
 </context>
 </TS>
